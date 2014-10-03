990af5db0dd26c32e8e4618307a393c8d5a865ff
jhenesi <hector.neyoy@gmail.com>
2014-10-03 12:08:06 -0700

    Primer commit. Yeoman Scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoturismoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the infoturismoApp
+ */
+angular.module('infoturismoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoturismoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the infoturismoApp
+ */
+angular.module('infoturismoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
